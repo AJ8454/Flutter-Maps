ffd4cf837ec18183e494b2959f3916111062d4e0
AJ8454 <ajaypangare8454@gmail.com>
2021-10-23 18:06:44 +0530

    first commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
@@ -13,18 +15,84 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   StreamSubscription? _locationSubscription;
-  Location _loactionTracker = Location();
+  final Location _locationTracker = Location();
   Marker? marker;
   Circle? circle;
   GoogleMapController? _controller;
 
-// initial position of map to load first i.e. I have given India.
+// initial position of map to load first
   static const CameraPosition initialLocation = CameraPosition(
-    target: LatLng(20.5937, 78.9629),
+    target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  void getCurrentLocation() async {}
+  Future<Uint8List> getMarker() async {
+    ByteData byteData =
+        await DefaultAssetBundle.of(context).load("assets/arrow_up.png");
+    return byteData.buffer.asUint8List();
+  }
+
+  void updateMarkerAndCircle(LocationData? newLocalData, Uint8List? imageData) {
+    LatLng latlng = LatLng(newLocalData!.latitude!, newLocalData.longitude!);
+    setState(() {
+      marker = Marker(
+        markerId: const MarkerId('home'),
+        position: latlng,
+        rotation: newLocalData.heading!,
+        draggable: false,
+        zIndex: 2,
+        flat: true,
+        anchor: const Offset(0.5, 0.5),
+        icon: BitmapDescriptor.fromBytes(imageData!),
+      );
+      circle = Circle(
+        circleId: const CircleId('arrow'),
+        radius: newLocalData.accuracy!,
+        zIndex: 1,
+        strokeColor: Colors.blue,
+        center: latlng,
+        fillColor: Colors.blue.withAlpha(70),
+      );
+    });
+  }
+
+  void getCurrentLocation() async {
+    try {
+      Uint8List imageData = await getMarker();
+      var location = await _locationTracker.getLocation();
+      updateMarkerAndCircle(location, imageData);
+
+      if (_locationSubscription != null) {
+        _locationSubscription!.cancel();
+      }
+
+      _locationSubscription =
+          _locationTracker.onLocationChanged.listen((newLocalData) {
+        if (_controller != null) {
+          _controller!
+              .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+            bearing: 192.8334901395799,
+            target: LatLng(newLocalData.latitude!, newLocalData.longitude!),
+            tilt: 0,
+            zoom: 18.00,
+          )));
+          updateMarkerAndCircle(newLocalData, imageData);
+        }
+      });
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        debugPrint('Permission Denied');
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    if (_locationSubscription != null) {
+      _locationSubscription!.cancel();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
